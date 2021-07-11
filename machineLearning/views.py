7216e95473e4ba4505e4932e66b57ee3270efa1a
souvik <ssouvik.191@gmail.com>
2021-07-12 01:11:56 +0530

    initial commit

---
+++ machineLearning/views.py
@@ -0,0 +1,25 @@
+from typing import ContextManager
+from django.shortcuts import HttpResponse, render
+import pickle
+
+def home(request):
+    new_predict = 0
+    predict = None
+    if request.method == 'POST':
+        room = request.POST.get('room')
+        bathroom = request.POST.get('bathroom')
+        Balcony = request.POST.get('Balcony')
+        SquareFt = request.POST.get('Square.Ft')
+        floor = request.POST.get('floor')
+        kolkata = request.POST.get('kolkata')
+        newtown = request.POST.get('newtown')
+        saltlake = request.POST.get('saltlake')
+        prediction = pickle.load(open('housePrice.pkl', 'rb'))
+        try:
+            new_predict=prediction.predict([[room, bathroom, Balcony, SquareFt, floor, kolkata, newtown, saltlake]])
+        except Exception as e:
+            print(e)
+       
+        
+    return render(request, 'home.html',{"predict": new_predict}) 
+    
